3a53a88630a255ba8b1b864263f4f3e7e4b4bf84
lcm1717 <lcm1623@tukorea.ac.kr>
2025-11-08 13:48:12 +0900

    이벤트 처리

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ from pico2d import*
 current_stage =1
 
 open_canvas()
-background_image= load_image('map1.png')
+background= load_image('map1.png')
 
 running = True
 while running:
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
+            running = False
+
+
