1d2d65bae1a155ce5876124d00af886be0d4ac54
lcm1717 <lcm1623@tukorea.ac.kr>
2025-11-12 13:30:52 +0900

    첫번째 몬스터 랜덤구현

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from pico2d import*
+import random
 
 current_stage =1
 
@@ -26,26 +27,51 @@ def handle_events():
             elif event.key == SDLK_LEFT:
                 dir +=1
 
+
+class monster:
+    def __init__(self):
+        self.x, self.y = random.randint(100,700), 90
+        self.frame = random.randint(0,9)
+        self.image = load_image('monster1.png')
+
+    def update(self):
+        self.frame = (self.frame + 1) % 10
+        self.x += 5
+
+    def draw(self):
+        self.image.clip_draw(self.frame * 100, 0, 100, 100, self.x, self.y)
+
+monsters = [Monster() for i in range(5)]
+
 running = True
 x= 800//2
 frame = 0
 dir = 0
+
 while running:
+    handle_events()
+    x += dir * 5
+    x = max(0, min(x, 800))
+
+    for monster in monsters:
+        monster.update()
     clear_canvas()
     background.draw(400,300,800,600)
+    for monster in monsters:
+        monster.draw()
 
     if dir>0:
-        run_character.clip_draw(frame*20,0,100,100,x,90)
+        run_character.clip_draw(frame*10,0,100,100,x,90)
+        frame =(frame+1) % 8
     elif dir<0:
-        run_character.clip_composite_draw(frame*70,0,100,100,0,'h',x,90,100,100)
+        run_character.clip_composite_draw(frame*10,0,100,100,0,'h',x,90,100,100)
+        frame = (frame + 1) % 8
     else:
-        character.clip_draw(frame*60,0,100,100,x,90)
+        character.clip_draw(frame*10,0,70,100,x,90)
+        frame = (frame + 1) % 5
+
     update_canvas()
-    handle_events()
-    frame =(frame+1)%5
-    x += dir *5
-    x=max(0,min(x,800))
-    delay(0.1)
+    delay(0.07)
 
 close_canvas()
 
